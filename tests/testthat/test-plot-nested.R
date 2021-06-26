cd91b9d0e071aa7ef3becb4dedded8509f49084b
Stefan Kloppenborg <stefan@kloppenborg.ca>
2021-06-25 19:52:50 -0600

    Updated to latest vidffr

--- tests/testthat/test-plot-nested.R
+++ tests/testthat/test-plot-nested.R
@@ -2,38 +2,41 @@ suppressMessages(library(dplyr))
 suppressMessages(library(ggplot2))
 
 
-expect_doppelganger <- function(title, fig, path = NULL, ...) {
-  testthat::skip_if_not_installed("vdiffr")
-  vdiffr::expect_doppelganger(title, fig, path = path, ...)
-}
-
-
-expect_doppelganger("nested_data_plot-no_grouping", {
-  carbon.fabric.2 %>%
-  filter(test == "WT") %>%
-  nested_data_plot(strength)
+test_that("nested_data_plot-no_grouping", {
+  skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger("nested_data_plot-no_grouping", {
+    carbon.fabric.2 %>%
+      filter(test == "WT") %>%
+      nested_data_plot(strength)
+  })
 })
 
-
-expect_doppelganger("nested_data_plot-single_grouping", {
-  carbon.fabric.2 %>%
-    filter(test == "WT" & condition == "RTD") %>%
-    nested_data_plot(strength,
-                     groups = c(batch))
+test_that("nested_data_plot-single_grouping", {
+  skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger("nested_data_plot-single_grouping", {
+    carbon.fabric.2 %>%
+      filter(test == "WT" & condition == "RTD") %>%
+      nested_data_plot(strength,
+                       groups = c(batch))
+  })
 })
 
-
-expect_doppelganger("nested_data_plot-two_groupings", {
-  carbon.fabric.2 %>%
-    filter(test == "WT" & condition == "RTD") %>%
-    nested_data_plot(strength,
-                     groups = c(batch, panel))
+test_that("nested_data_plot-two_groupings", {
+  skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger("nested_data_plot-two_groupings", {
+    carbon.fabric.2 %>%
+      filter(test == "WT" & condition == "RTD") %>%
+      nested_data_plot(strength,
+                       groups = c(batch, panel))
+  })
 })
 
-
-expect_doppelganger("nested_data_plot-single_obs_per_group", {
-  carbon.fabric.2 %>%
-    filter(test == "WT" & condition == "RTD") %>%
-    nested_data_plot(strength,
-                     groups = c(batch, panel, strength))
+test_that("nested_data_plot-single_obs_per_group", {
+  skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger("nested_data_plot-single_obs_per_group", {
+    carbon.fabric.2 %>%
+      filter(test == "WT" & condition == "RTD") %>%
+      nested_data_plot(strength,
+                       groups = c(batch, panel, strength))
+  })
 })
