6f8939812e2bfee4792a23171980d01041312a0b
Stefan Kloppenborg <stefan@kloppenborg.ca>
2025-03-18 20:35:32 -0600

    Disable return_linter

--- .lintr.R
+++ .lintr.R
@@ -1,7 +1,8 @@
 linters <- linters_with_defaults(
   line_length_linter = NULL,
   trailing_whitespace_linter = NULL,
-  indentation_linter = NULL
+  indentation_linter = NULL,
+  return_linter = NULL
 )
 # Ignore the auto-generated R files
 exclusions <- list(
